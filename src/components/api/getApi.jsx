0d928a58690c17c1226ed3fd9b6ae8677e3088bc
Adebisi Oluwarotimi <68323182+oluwarotimi1@users.noreply.github.com>
2023-11-09 00:16:37 +0100

    Update

--- src/components/api/getApi.jsx
+++ src/components/api/getApi.jsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export function getBtc() {
   return axios.get("https://blockchain.info/ticker").then((res) => res.data);
-};
+}
 
--- src/pages/maindashboard/MainDashboard.jsx
+++ src/pages/maindashboard/MainDashboard.jsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React  from "react";
 import styles from "./maindashboard.module.css";
 import BuySection from "../../components/dashboardpage/buysection/BuySection";
 import Marquee from "react-fast-marquee";
 import DashboardNav from "../../components/dashboardnav/DashboardNav";
 import Orders from "../../components/dashboardpage/orders/Orders";
 import PortfoilioSection from "../../components/dashboardpage/portfoiliosection/PortfoilioSection";
+import { useQuery } from "@tanstack/react-query";
+import { getBtc } from "../../components/api/getApi";
+
+
 
 
 const MainDashboard = () => {
+ 
 
-    // const getPrice=()=> {
-    //     return axios.get("https://blockchain.info/ticker").then((res) => res.data)
-    //     // console.log((res))
-    //     ;
-    //   }
+      const postsQuery = useQuery({
+        queryKey: ["btc"],
+        queryFn: getBtc,
+        refetchInterval: 1000,
+      });
+
+
+
   return (
     <div className={styles.container}>
       <section className={styles.main_dashboard}>
         <nav >
           <DashboardNav />
-          {/* <button onClick={getPrice}>get</button> */}
         </nav>
         <header className={styles.main_dashboard_header}>
           <Marquee >
-            Hello!
+            BTC:  {postsQuery.data && (
+                  <span style={{ color: "green" }}>
+                    {" "}
+                    ${postsQuery.data.USD.last}
+                  </span>
+                )}
           </Marquee>
         </header>
         <main>
