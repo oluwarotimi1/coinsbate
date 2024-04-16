6294f3448d2d6eecd208548e41890762c1843b77
Adebisi Oluwarotimi <68323182+oluwarotimi1@users.noreply.github.com>
2024-04-16 10:09:35 +0100

    Update

--- src/components/dashboardcomps/helpsupport/HelpSupport.jsx
+++ src/components/dashboardcomps/helpsupport/HelpSupport.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./helpsupport.module.css";
 const HelpSupport = () => {
   return (
-    <div>
+    <div className={styles.help_container}>
       <h4>
-      Contact us for help/support by sending us an email at coinsbate@gmail.com
-   
+        Contact us for help/support by sending us an email at
+        coinsbate@gmail.com
       </h4>
-      </div>
-  )
-}
 
-export default HelpSupport
+      <button className={styles.btn_contact_us} onClick={() => (window.location = "mailto:coinsbate@gmail.com")}>
+        Contact Us
+      </button>
+    </div>
+  );
+};
+
+export default HelpSupport;
